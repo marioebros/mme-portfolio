cb85a0e30c76944a949173f368814899d1665b71
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-10 18:03:45 -0800

    Input correct links + projects/work experience

--- constants/data.js
+++ constants/data.js
@@ -1,43 +1,43 @@
 const userData = {
-  githubUsername: "manuarora700",
-  name: "Manu Arora",
-  designation: "Full-Stack Developer",
-  avatarUrl: "/avatar.png",
-  email: "manuarorawork@gmail.com",
-  phone: "+91 9587738861",
-  address: "Jaipur, Rajasthan, India.",
+  githubUsername: "marioebros",
+  name: "Mario M. Elizalde",
+  designation: "Software Developer",
+  avatarUrl: "",
+  email: "mariomelizalde@gmail.com",
+  phone: "+1 805-791-5089",
+  address: "Simi Valley, CA",
   projects: [
     {
-      title: "Tailwind Master Kit",
-      link: "https://tailwindmasterkit.com",
+      title: "Portfolio | Built w/TypeScript",
+      link: "https://github.com/marioebros/portfolio",
       imgUrl: "/tmk.jpg",
     },
     {
-      title: "PlaceholderTech",
-      link: "https://placeholdertech.in",
+      title: "Weather App",
+      link: "https://github.com/marioebros/weather-app",
       imgUrl: "/placeholdertech.png",
     },
     {
-      title: "Portfolio",
-      link: "https://manuarora.in",
+      title: "toDo List",
+      link: "https://github.com/marioebros/toDo-list",
       imgUrl: "/portfolio.png",
     },
     {
-      title: "CovidRescue",
-      link: "https://covidrescue.co.in",
+      title: "Dex Stacks",
+      link: "https://github.com/marioebros/dex-stacks",
       imgUrl: "/covidrescue.png",
     },
     {
-      title: "Algochurn",
-      link: "https://algochurn.com",
+      title: "Recursion",
+      link: "https://github.com/marioebros/recursion",
       imgUrl: "/algochurn.png",
     },
     {
-      title: "Playground",
-      link: "https://play.placeholdertech.in",
+      title: "Blockchain",
+      link: "https://github.com/marioebros/blockchain",
       imgUrl: "/playground.png",
     },
-    {
+    /* {
       title: "Feedmeback",
       link: "https://feedmeback.vercel.app",
       imgUrl: "/feedmeback.png",
@@ -46,71 +46,88 @@ const userData = {
       title: "Devmedium",
       link: "https://devmedium.vercel.app",
       imgUrl: "/devmedium.png",
-    },
+    }, */
   ],
   about: {
-    title:
-      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    title: "Software Developer | Web3 Dev | Keep it simple.",
     description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `Hi, I'm Mario the Self Taught Software Developer & Web3 Enthusiast!
+
+      Graduate from The Odin Project's Fullstack JavaScript Course
+      
+       - Graduate from Boston University with a Master of Music Degree
+      
+       - Graduate from California Lutheran University with a Bachelor of Arts Degree
+      
+      Working towards one day working as a senior developer & creating opportunities for other first-generation musicians, engineers, and developers. 
+      
+      In my free time you can find me playing the trombone or watching a scary movie. 
+      
+      JavaScript | TypeScript | HTML/CSS | React | Next.js | MongoDB | Fluent Spanish
+      
+      Interested? Don't hesitate to reach out! Also, check out what I've been working on! 
+      
+      ➣ https://github.com/marioebros
+      
+      Best,
+      Mario M. Elizalde`,
     ],
-    currentProject: "Tailwind Master Kit",
-    currentProjectUrl: "https://tailwindmasterkit.com",
+    currentProject: "GitHub Repo for Mario M. Elizalde,
+    currentProjectUrl: "https://github.com/marioebros",
   },
   experience: [
     {
       title: "Software Developer",
-      company: "mroads",
-      year: "2021",
+      company: "Mario M. Elizalde",
+      year: "2022 to Present",
       companyLink: "https://mroads.com",
       desc: "Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.",
     },
     {
-      title: "Founder and Developer",
-      company: "TailwindMasterKit",
-      year: "2021",
+      title: "Email Copywriter",
+      company: "Mario M. Elizalde",
+      year: "2020 to Present",
       companyLink: "https://tailwindmasterkit.com",
       desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
     },
     {
-      title: "Founder and Developer",
-      company: "Algochurn",
-      year: "2021",
+      title: "Musician/Recording Engineer",
+      company: "Mario M. Elizalde",
+      year: "2012 to 2021",
       companyLink: "https://algochurn.com",
       desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
     },
     {
-      title: "Internship",
-      company: "Intel Corporation",
-      year: "2019",
+      title: "Roommate Matchmaker",
+      company: "Roomeze",
+      year: "2019 to 2020",
       companyLink: "https://intel.com",
       desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
     },
     {
-      title: "Graduation",
-      company: "UPES, Dehradun.",
-      year: "2019",
+      title: "Operations Assistant",
+      company: "Boston University, Agganis Arena.",
+      year: "2017 to 2018",
       companyLink: "https://upes.co.in",
       desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
     },
     {
-      title: "High School",
-      company: "Delhi Public School, Jaipur.",
-      year: "2015",
+      title: "Music Department Assistant",
+      company: "California Lutheran University",
+      year: "2014 to 2016",
       companyLink: "https://upes.co.in",
       desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
     },
   ],
-  resumeUrl:
-    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  resumeUrl: "../assets/marioMElizalde_resume_site.pdf",
+  /* resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing", */
   socialLinks: {
-    instagram: "https://instagram.com/maninthere",
-    twitter: "https://twitter.com/mannupaaji",
-    linkedin: "https://linkedin.com/in/manuarora28",
-    github: "https://github.com/manuarora700",
-    facebook: "https://facebook.com/",
+    instagram: "https://instagram.com/issa_mmeh",
+    twitter: "https://twitter.com/issa_mmeh",
+    linkedin: "https://www.linkedin.com/in/mariomeliz/",
+    github: "https://github.com/marioebros",
+    // facebook: "https://facebook.com/",
   },
 };
 
